7cebad54520cbd68513b14ee6c7888e7f7950908
valkln <kalinin.valentyn96@gmail.com>
2021-09-19 13:47:44 +0300

    upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	$('.branding__slider').slick({
+		arrows: false,
+		dots: true,
+		autoplay: true,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true
+	});
+});
+
+$(document).ready(function () {
+	$('.testimonials__slider').slick({
+		arrows: false,
+		dots: true,
+		autoplay: true,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true
+	});
+});
